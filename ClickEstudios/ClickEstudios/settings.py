c2e94ed73d4475bd9afab9fbc5be74cdb0f09801
Wandy Olivares <untal.wandy@gmail.com>
2024-12-28 03:06:43 -0400

    Activar el modo DEBUG en la configuración para facilitar la depuración durante el desarrollo.

--- ClickEstudios/ClickEstudios/settings.py
+++ ClickEstudios/ClickEstudios/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#a!t#rktx*5ut^&7fs0@xt-p6m)shb3pwnz8njg7w&78)dk0ee'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
