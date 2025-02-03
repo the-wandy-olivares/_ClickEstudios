a80ebb609c8d0514ac23699ea85a86b33b7a19b3
Wandy Olivares <user.email>
2025-02-03 13:03:30 -0400

    Correcciones

--- ClickEstudios/ClickEstudios/urls.py
+++ ClickEstudios/ClickEstudios/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
 
 
 
@@ -10,7 +11,6 @@ urlpatterns = [
     path('' , include('Estudios.urls')),
     path('admin/', admin.site.urls),
 ]
-from django.conf.urls import handler404
 
 handler404 = 'Estudios.views.Status404'
 
