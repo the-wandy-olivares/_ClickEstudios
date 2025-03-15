b10d3e95345f0d96fc94604bce53fe78f7120dbe
Wandy Olivares <user.email>
2025-03-15 17:24:25 -0400

    Mejorar la lógica de preguntas en el asistente: agregar manejo de citas y optimizar la respuesta de ventas; actualizar estilos en la plantilla del asistente.

--- ClickEstudios/Asistente/views_ajax.py
+++ ClickEstudios/Asistente/views_ajax.py
@@ -29,20 +29,37 @@ def Obtener_Total_Ingresos():
 
 
 def CleanQuestion(question):
-      if 'vent' in question.lower():
+      if 'vent'  in question.lower():
             sale = Sale.objects.all().order_by('-id')[:50]
             sales_data = []
             for s in sale:
                   sales_data.append({
                         'name_plan': s.name_plan if s.name_plan else 'N/A',
-                        'price_plan': f"{s.price_plan:,}",
-                        'payment': s.payment,
+                        'payment': s.payment if 'Si' else 'No',
                         'name_client': s.name_client if s.name_client else 'N/A',
                         'email_client': s.email_client if s.email_client else 'N/A',
                         'phone_client': s.phone_client if s.phone_client else 'N/A',
-                        'date': s.date.strftime('%d-%m-%Y %H:%M:%S'),
+                        'dia_de_la_cita': s.date_choice,
+                        'monto_faltante': f"{s.debit_mount:,}",
+                        'monto_pagado': f"{s.mount:,}",
                   })
             return f'{sales_data}'
+      
+      elif 'citas' in question.lower():
+            citas = Sale.objects.filter(is_reserve=True, finalize=False).order_by('-id')[:50]
+            citas_data = []
+            for s in citas:
+                  citas_data.append({
+                        'name_plan': s.name_plan if s.name_plan else 'N/A',
+                        'payment': s.payment if 'Si' else 'No',
+                        'name_client': s.name_client if s.name_client else 'N/A',
+                        'email_client': s.email_client if s.email_client else 'N/A',
+                        'phone_client': s.phone_client if s.phone_client else 'N/A',
+                        'dia_de_la_cita': s.date_choice,
+                        'monto_faltante': f"{s.debit_mount:,}",
+                        'monto_pagado': f"{s.mount:,}",
+                  })
+            return citas_data 
       
       elif 'plan' in question.lower() :
             plan_data = []
@@ -75,18 +92,21 @@ def QuestionGemini(request):
       data = {'data': False}
       if your_question:
             client = genai.Client(api_key="AIzaSyAlRiEbU6bhninS9aZvaO5MfB8jWyzfjw0")
-
             if CleanQuestion(your_question)  != False:
-                  question = your_question + CleanQuestion(your_question) 
+                  question =   CleanQuestion(your_question) 
             else:
                   question = your_question
             
+            # asisten_response = CleanQuestion(your_question)
+            # print(CleanQuestion(your_question))
+
             asisten_response = client.models.generate_content(
             model="gemini-2.0-flash",
             contents = f"""
                   Sistema: ClickEstudios
                   Contexto: Click, Asistente de un estudio y das respuestas puntuales.
-                  Objetivo: Proporcionar información detallada sobre planes, servicios, ventas, comparaciones y de mas.
+                  Objetivo: Proporcionar información detallada sobre planes, servicios, ventas, citas comparaciones y de mas.
+                  Fecha de hoy: {datetime.now().strftime('%d/%m/%Y')}
                   Pregunta:  {question}
                   """,)
             
