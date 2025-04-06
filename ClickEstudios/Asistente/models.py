3afa7f8f0f86f67769b907865fb0ffdc46c9817b
Wandy Olivares <user.email>
2025-04-06 15:32:13 -0400

    Mejoras de diseño y renovacion de la app asistente

--- ClickEstudios/Asistente/models.py
+++ ClickEstudios/Asistente/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Configuration(models.Model):
+      AI_CHOICES = [
+            ('gemini', 'Géminis'),
+            ('chatgpt', 'Chat-GPT'),
+            ('llama', 'Llama'),
+            ('gemma', 'Gemma')
+      ]
+
+      model = models.CharField(max_length=10, choices=AI_CHOICES, default='gemini')
+      is_local = models.BooleanField(default=False)
+
+      
+      def __str__(self):
+            return f"Configuración: {self.get_model_display()}"
+      
+
+
+class ModelsIA(models.Model):
+      name = models.CharField(max_length=90, default='local', blank=True, null=True)
+      api_keys = models.TextField(default='', blank=True, null=True)
+
+      is_active = models.BooleanField(default=False)
+
+      
+      def __str__(self):
+            return f"{self.name} - {self.is_active}"
+      
+
+
+
